3d182f1c3c054634cb55b286cc77e31302972fb6
MarwaBadr721 <marwa.badr@f-eng.tanta.edu.eg>
2024-11-29 09:42:17 +0200

    Update cardslice.jsx

--- cardslice.jsx
+++ cardslice.jsx
@@ -1,1 +1,20 @@
 
+import { createSlice } from '@reduxjs/toolkit';
+export const housesSlice = createSlice({
+  name: 'houses',
+  initialState: [
+    { name: 'Breakfast', cost: 50, selected: false },
+{ name: 'High Tea', cost: 25, selected: false },
+{ name: 'Lunch', cost: 65, selected: false },
+{ name: 'Dinner', cost: 70, selected: false },
+   
+  ],
+  reducers: {
+    togglehouseSelection: (state, action) => {
+        state[action.payload].selected = !state[action.payload].selected;
+  },
+  },
+});
+
+export const { togglehouseSelection } = housesSlice.actions;
+export default housesSlice.reducer;
