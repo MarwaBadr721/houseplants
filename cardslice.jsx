607e0c3bcb069e7b159cd5cd7e1277be33b04e21
MarwaBadr721 <marwa.badr@f-eng.tanta.edu.eg>
2024-11-29 09:55:39 +0200

    Update cardslice.jsx

--- cardslice.jsx
+++ cardslice.jsx
@@ -3,10 +3,12 @@ import { createSlice } from '@reduxjs/toolkit';
 export const housesSlice = createSlice({
   name: 'houses',
   initialState: [
-    { name: 'Breakfast', cost: 50, selected: false },
-{ name: 'High Tea', cost: 25, selected: false },
-{ name: 'Lunch', cost: 65, selected: false },
-{ name: 'Dinner', cost: 70, selected: false },
+    { name: 'one', cost: 50000, selected: false },
+{ name: 'two', cost: 25000, selected: false },
+{ name: 'three', cost: 65000, selected: false },
+{ name: 'four', cost: 70000, selected: false },
+    { name: 'five', cost: 70000, selected: false },
+    { name: 'six', cost: 70000, selected: false },
    
   ],
   reducers: {
