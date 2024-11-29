38e8d99108c11f638f07692839a55cff1a5e84df
MarwaBadr721 <marwa.badr@f-eng.tanta.edu.eg>
2024-11-29 09:58:34 +0200

    Update stor_redux.jsx

--- stor_redux.jsx
+++ stor_redux.jsx
@@ -1,1 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import venueReducer from './cardslice';
+import avReducer from './avSlice';
+import mealsReducer from './houseSlice';
 
+export default configureStore({
+  reducer: {
+    venue: venueReducer,
+    av: avReducer,
+    meals: mealsReducer,
+  },
+});
